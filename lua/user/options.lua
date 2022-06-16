af821497974bed497dfa39c3d870913dc15ce5cb
Stefan Bondzulic <stefanbondzulic@gmail.com>
2022-06-16 14:57:14 +0200

    Batter option syntax

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -42,12 +42,11 @@ local options = {
   cmdheight = 0, -- By defauilt hide command line
 }
 
-vim.opt.shortmess:append "c"
-
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+-- Appends
+vim.opt.shortmess:append "c"
+vim.opt.whichwrap:append "<,>,[,],h,l"
+vim.opt.iskeyword:append "-"
